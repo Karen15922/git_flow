7552632d9283874a1e7465e15cd563ac55792c2d
Карен <karen.karakeyan@yandex.ru>
2024-03-26 12:13:07 +0300

    add docstring

--- str_func.py
+++ str_func.py
@@ -1,4 +1,9 @@
 def to_upper(s):
+    """Функция переводит в верхний регистр"""
     return s.upper()
+
+def capitalize_words(s):
+    """Функция переводит первую в заглавную каждого слова в строке """
+    return ' '.join(word.capitalize() for word in s.split())
 
 
